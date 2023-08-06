f18e1a26db13ca78b1022263c73983036d42565d
Romy Gaus <romy.p.gaus@gmail.com>
2023-08-06 10:53:48 +0200

    rename list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import List from './components/list/List.js';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
           <h1>Overview</h1>
           <h4>Organize your supplies and find out what you need to buy soon!</h4>
         </div>
-
+        <List></List>
       </div>
     )
   }
