a1ed7830c4379e91bb4c6bf2d3b48a9cf05253cf
Romy Gaus <romy.p.gaus@gmail.com>
2023-08-06 13:57:52 +0200

    render list

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,34 +1,49 @@
-import React, { Component } from 'react';
-import './List.css';
+import React, { Component } from "react";
+import "./List.css";
 
 class List extends Component {
-    constructor() {
-        super()
-        this.state = {
-            items: []
-        }
-      }
-
-      componentDidMount() {
-        fetch("http://localhost:8000/stocks").then((res) => {
-            return res.json();
-        }) 
-        .then((json) => {
-            this.setState({
-                items: json.data
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  constructor() {
+    super();
+    this.state = {
+      items: [],
     };
+  }
 
-    render() {
-        console.log(this.state.items)
-        return(
-            <div></div>
-        )
-    }
+  componentDidMount() {
+    fetch("http://localhost:8000/stocks")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({
+          items: json.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="list-container">
+        <div className="list-row list-header">
+            <p>Name</p>
+            <p>Amount</p>
+            <p>Target</p>
+            <p>Updated</p>
+          </div>
+        {Object.entries(this.state.items).map(([key, value]) => (
+          <div className="list-row">
+            <p>{value.name}</p>
+            <p>{value.amount}</p>
+            <p>{value.target}</p>
+            <p>{value.updated}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default List;
