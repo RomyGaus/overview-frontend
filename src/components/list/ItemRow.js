8591e22e4d6f67b4138eec1e30c00b59fff013c0
Romy Gaus <romy.p.gaus@gmail.com>
2023-08-12 17:29:02 +0200

    extract row component

---
+++ src/components/list/ItemRow.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import "./List.css";
+
+class ItemRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        id: props.value.id,
+        name: props.value.name,
+        amount: props.value.amount,
+        target: props.value.target,
+        updated: props.value.updated
+    };
+  }
+
+  formatDate = (epoch) => {
+    const d = new Date(epoch);
+    let month = d.getMonth() + 1;
+    let day = d.getDate();
+    let year = d.getFullYear();
+
+    let newdate = year + "/" + month + "/" + day;
+    let text = newdate.toString();
+    return text;
+  };
+
+  updateItem = (item) => {
+    fetch(`http://localhost:8000/stocks/${this.state.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(item),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        let newState = item
+        item.updated = json.data.updated
+        this.setState(newState);
+      });
+  };
+
+  render() {
+    return (
+      <div className="list-row" key={this.key}>
+        <div>
+          <input
+            className="name"
+            type="text"
+            name=""
+            defaultValue={this.state.name}
+          />
+          <button className="btn btn-edit">&#9998;</button>
+          <button
+            className="btn btn-save"
+            onClick={() =>
+              this.updateItem({ name: this.state.name })
+            }
+          >
+            &#x2713;
+          </button>
+        </div>
+        <div className="amount">
+          <input
+            type="number"
+            name=""
+            min="0"
+            max="100"
+            value={this.state.amount}
+            readOnly
+          />
+          <button
+            className="btn arrow arrow-up"
+            onClick={() =>
+              this.updateItem({
+                amount: this.state.amount + 1
+              })
+            }
+          >
+            &#9650;
+          </button>
+          <button
+            className="btn arrow arrow-down"
+            onClick={() =>
+              this.updateItem({
+                amount: this.state.amount - 1
+              })
+            }
+          >
+            &#9660;
+          </button>
+        </div>
+        <div className="target">
+          <input
+            type="number"
+            name=""
+            min="0"
+            max="100"
+            value={this.state.target}
+            readOnly
+          />
+          <button
+            className="btn arrow arrow-up"
+            onClick={() =>
+              this.updateItem({
+                target: this.state.target + 1
+              })
+            }
+          >
+            &#9650;
+          </button>
+          <button
+            className="btn arrow arrow-down"
+            onClick={() =>
+              this.updateItem({
+                target: this.state.target - 1
+              })
+            }
+          >
+            &#9660;
+          </button>
+        </div>
+        <p className="num">{this.formatDate(this.state.updated)}</p>
+        <img
+          className="btn btn-delete"
+          src={require("./delete.png")}
+          alt="delete-icon"
+          onClick={() => this.props.onDelete(this.state.id)}
+        ></img>
+      </div>
+    );
+  }
+}
+
+export default ItemRow;
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import ItemRow from "./ItemRow";
 import "./List.css";
 
 class List extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       items: [],
     };
@@ -24,76 +25,38 @@ class List extends Component {
       });
   }
 
-  formatDate = (epoch) => {
-    const d = new Date(epoch);
-    let month = d.getMonth() + 1;
-    let day = d.getDate();
-    let year = d.getFullYear();
-
-    let newdate = year + "/" + month + "/" + day;
-    let text = newdate.toString();
-    return text;
-  }
-
   newItem = () => {
     fetch("http://localhost:8000/stocks/", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name: "",
-            amount: 0,
-            target: 0
-        })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: "",
+        amount: 0,
+        target: 0,
+      }),
     })
-    .then(response => response.json())
-    .then((json) => {
-        var updatedStateItems = this.state.items
-        updatedStateItems.push(json.data)
+      .then((response) => response.json())
+      .then((json) => {
+        var updatedStateItems = this.state.items;
+        updatedStateItems.push(json.data);
         this.setState({
-            items: updatedStateItems
-        })
-    })
-  }
-
-  updateItem = (item) => {
-    var updatedItem = this.state.items.find((element) => element.id === item.id);
-    Object.keys(updatedItem).forEach(key => {
-        if(item[key] !== undefined) {
-            updatedItem[key] = item[key]
-        }
-    })
-    fetch(`http://localhost:8000/stocks/${item.id}`, {
-        method: "PATCH",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedItem)
-    })
-    .then(response => response.json())
-    .then((json) => {
-        var updatedStateItems = this.state.items
-        var index = updatedStateItems.findIndex((element) => element.id === item.id);
-
-        if (index >= 0) {
-            updatedStateItems[index] = updatedItem;
-        }
-        this.setState({
-            items: updatedStateItems
-        })
-    })
-  }
+          items: updatedStateItems,
+        });
+      });
+  };
 
   deleteItem = (id) => {
     fetch(`http://localhost:8000/stocks/${id}`, {
-        method: "DELETE",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify()
-    })
-    .then((res) => {
-        var updatedStateItems = this.state.items.filter(item => item.id !== id)
-        this.setState({
-            items: updatedStateItems
-        })
-    })
-  }
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(),
+    }).then((res) => {
+      var updatedStateItems = this.state.items.filter((item) => item.id !== id);
+      this.setState({
+        items: updatedStateItems,
+      });
+    });
+  };
 
   render() {
     return (
@@ -105,25 +68,7 @@ class List extends Component {
             <p className="num">Updated</p>
         </div>
         {Object.entries(this.state.items).map(([key, value]) => (
-          <div className="list-row" key={key}>
-            <div>
-                <input className="name" type="text" name="" defaultValue={value.name}/>
-                <button className="btn btn-edit">&#9998;</button>
-                <button className="btn btn-save" onClick={() => this.updateItem({id: value.id, name: value.name})}>&#x2713;</button>
-            </div>
-            <div className="amount">
-                <input type="number" name="" min="0" max="100" defaultValue={value.amount}/>
-                <button className="btn arrow arrow-up" onClick={() => this.updateItem({id: value.id, amount: value.amount + 1})}>&#9650;</button>
-                <button className="btn arrow arrow-down" onClick={() => this.updateItem({id: value.id, amount: value.amount - 1})}>&#9660;</button>
-            </div>
-            <div className="target">
-                <input type="number" name="" min="0" max="100" defaultValue={value.target}/>
-                <button className="btn arrow arrow-up" onClick={() => this.updateItem({id: value.id, target: value.target + 1})}>&#9650;</button>
-                <button className="btn arrow arrow-down" onClick={() => this.updateItem({id: value.id, target: value.target - 1})}>&#9660;</button>
-            </div>
-            <p className="num">{this.formatDate(value.updated)}</p>
-            <img className="btn btn-delete" src={require('./delete.png')} alt="delete-icon" onClick={() => this.deleteItem(value.id)}></img>
-          </div>
+          <ItemRow key = {key} value = {value} onDelete={this.deleteItem}></ItemRow>
         ))}
         <button className="btn btn-new" onClick={() => this.newItem()}>New</button>
       </div>
